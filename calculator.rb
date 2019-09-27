b1b95a675101aebb495dbc07d81973f351b794b2
Rachid Rakaa <rachid1982fsb@gmail.com>
2019-09-27 23:29:49 +0000

    Automatically backed up by Learn

--- calculator.rb
+++ calculator.rb
@@ -1,7 +1,7 @@
 # Add your variables here
 
-first_number = Float
-second_number = Float
+first_number = 1
+second_number = 2
 sum = first_number+second_number
 difference =first_number - second_number
 product= first_number*second_number
